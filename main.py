5041b9239bc4362753d4d8702f0559fcc2386a0e
David Liberal <davidliberal0@gmail.com>
2024-09-16 10:15:14 -0700

    style clock widget text and background

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ from PyQt5.QtCore import QTimer, QTime, Qt
 class DigitalClock(QWidget): 
     def __init__(self):
         super().__init__()
-        self.time_label = QLabel(self)
+        self.time_label = QLabel("12:00:00", self)
         self.timer = QTimer(self)
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle("Digital Clock")
         self.setGeometry(600, 400, 300, 100) # x-axis, y-axis, width, height 
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.time_label)
+        self.setLayout(vbox)
+
+        self.time_label.setAlignment(Qt.AlignCenter)
+        self.time_label.setStyleSheet("font-size: 150px;"
+                                      "font-family: Arial;"
+                                      "color: #26ff00;")
+        self.setStyleSheet("background-color: black;")
 
 
 
